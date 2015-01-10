4337d80e12029507cea685f85c64cba465aa71c0
jiebour <jiebour@hotmail.com>
2015-01-10 08:49:02 -0800

    to启亮&强子

--- protocol.php
+++
@@ -1,7 +0,0 @@
-<?php
-	function get_user_protocol($user) {
-		return array('id'=>$user->id,
-				'name'=>$user->name, 'id'=>$user->head,
-				'sex'=>$user->sex, 'location'=>$user->location, );
-	}
-?>
